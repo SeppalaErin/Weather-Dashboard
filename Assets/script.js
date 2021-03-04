454ea81374b2a39ed151dae548d316ba106fe4fa
Erin Seppala <aseppala98@gmail.com>
2021-03-03 22:01:15 -0500

    Finished script.js

--- Assets/script.js
+++ Assets/script.js
@@ -10,7 +10,36 @@ let recentCities = [];
 let apiKey = "5178dc0fce8514b75d7a89420078fe33";
 
 searchBtn.on("click", () => {
-    let zipCode = searchInput.val();
+  let zipCode = searchInput.val();
+  let lat;
+  let lon;
+  let fiveDayLink = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&appid=${apiKey}&units=imperial`;
+  fetch(fiveDayLink)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw Error("Invalid Zip Code, Please Enter a Valid Zip Code.");
+      }
+    })
+    .then((data) => {
+      populateOneDay(data);
+      checkRecentCities(data, zipCode);
+      populateFiveDay(data);
+      writeRecentCities();
+      lat = data.city.coord.lat;
+      lon = data.city.coord.lon;
+      let oneCallLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=${apiKey}`;
+      fetchUV(oneCallLink);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
+
+$(document).on("click", function (e) {
+  if (e.target.id == "recent-city") {
+    let zipCode = e.target.getAttribute("zip");
     let lat;
     let lon;
     let fiveDayLink = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&appid=${apiKey}&units=imperial`;
@@ -18,14 +47,12 @@ searchBtn.on("click", () => {
       .then((response) => {
         if (response.ok) {
           return response.json();
-        } else {
-          throw Error("Invalid Zip Code, Please Enter a Valid Zip Code.");
-        }
+        } 
       })
       .then((data) => {
+        populateFiveDay(data);
         populateOneDay(data);
         checkRecentCities(data, zipCode);
-        populateFiveDay(data);
         writeRecentCities();
         lat = data.city.coord.lat;
         lon = data.city.coord.lon;
@@ -35,123 +62,135 @@ searchBtn.on("click", () => {
       .catch((error) => {
         alert(error);
       });
-  });
-  
-  $(document).on("click", function (e) {
-    if (e.target.id == "recent-city") {
-      let zipCode = e.target.getAttribute("zip");
-      let lat;
-      let lon;
-      let fiveDayLink = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&appid=${apiKey}&units=imperial`;
-      fetch(fiveDayLink)
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } 
-        })
-        .then((data) => {
-          populateFiveDay(data);
-          populateOneDay(data);
-          checkRecentCities(data, zipCode);
-          writeRecentCities();
-          lat = data.city.coord.lat;
-          lon = data.city.coord.lon;
-          let oneCallLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=${apiKey}`;
-          fetchUV(oneCallLink);
-        })
-        .catch((error) => {
-          alert(error);
-        });
-    }
-  });
-  
-  function fetchUV(link) {
-    fetch(link)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } 
-      })
-      .then((data) => {
-        let uv = $("<p>");
-        let indexValue = $("<span>");
-        indexValue.text(data.current.uvi);
-        colorUv(data.current.uvi, indexValue);
-        uv.addClass("card-text result-p");
-        uv.text(`UV Index: `);
-        uv.append(indexValue);
-        oneDayResult.append(uv);
-      })
-      .catch((error) => {
-        alert(error);
-      });
   }
-  
-  function colorUv(val, el) {
-    if (val < 3) {
-      el.addClass("uv-low");
-    } else if (val < 6) {
-      el.addClass("uv-med");
-    } else if (val < 10) {
-      el.addClass("uv-high");
-    } else {
-      el.addClass("uv-danger");
+});
+
+function fetchUV(link) {
+  fetch(link)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } 
+    })
+    .then((data) => {
+      let uv = $("<p>");
+      let indexValue = $("<span>");
+      indexValue.text(data.current.uvi);
+      colorUv(data.current.uvi, indexValue);
+      uv.addClass("card-text result-p");
+      uv.text(`UV Index: `);
+      uv.append(indexValue);
+      oneDayResult.append(uv);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
+
+function colorUv(val, el) {
+  if (val < 3) {
+    el.addClass("uv-low");
+  } else if (val < 6) {
+    el.addClass("uv-med");
+  } else if (val < 10) {
+    el.addClass("uv-high");
+  } else {
+    el.addClass("uv-danger");
+  }
+}
+
+function writeRecentCities() {
+  listGroup.empty();
+  for (let i = 0; i < recentCities.length; i++) {
+    let html = `<li class="list-group-item recent-city" id="recent-city" zip="${recentCities[i].zip}">${recentCities[i].name}</li>`;
+    listGroup.append(html);
+  }
+}
+
+function checkRecentCities(data, zip) {
+  let city = {
+    name: data.city.name,
+    zip: zip,
+  };
+
+  for (let i = 0; i < recentCities.length; i++) {
+    if (recentCities[i].zip == zip) {
+      recentCities.splice(i, 1);
     }
   }
-  
-  function writeRecentCities() {
-    listGroup.empty();
-    for (let i = 0; i < recentCities.length; i++) {
-      let html = `<li class="list-group-item recent-city" id="recent-city" zip="${recentCities[i].zip}">${recentCities[i].name}</li>`;
-      listGroup.append(html);
+
+  if (recentCities.length < 5) {
+    recentCities.unshift(city);
+  } else {
+    recentCities.pop();
+    recentCities.unshift(city);
+  }
+
+  localStorage.setItem("recent", JSON.stringify(recentCities));
+}
+
+function init() {
+  if (localStorage.getItem("recent") !== null) {
+    let retrievedData = localStorage.getItem("recent");
+    recentCities = JSON.parse(retrievedData);
+    writeRecentCities();
+  }
+}
+
+function populateOneDay(data) {
+  oneDayResult.removeClass("hide");
+  oneDayResult.empty();
+  let cityName = data.city.name;
+  let listItem = data.list[0];
+  let id = data.list[0].weather[0].id;
+  id = id.toString();
+
+
+  let html =
+    `<h2>${cityName} ${unixToDate(listItem.dt)}</h2>` +
+    `<p class="result-p">Temp: ${listItem.main.temp.toFixed(0)}&degF</p>` +
+    `<p class="result-p">Humidity: ${listItem.main.humidity}%</p>` +
+    `<p class="result-p">Wind: ${listItem.wind.speed} MPH</p>`;
+
+  oneDayResult.append(html);
+}
+
+function populateFiveDay(data) {
+  fiveDayTitle.removeClass("hide");
+  fiveDayResult.empty();
+  let dataList = data.list;
+  for (let i = 0; i < dataList.length; i++) {
+    if (unixToHour(dataList[i].dt) > 4 && unixToHour(dataList[i].dt) < 8) {
+      let listItem = dataList[i];
+      let html =
+        `<div class="col-2 card">` +
+        `<h5 class="card-title">${unixToDate(dataList[i].dt)}</h5>` +
+        `<div class="card-text">` +
+        `<img src="https://openweathermap.org/img/wn/${listItem.weather[0].icon}@2x.png" alt="Weather Icon"/>` +
+        `</div>` +
+        `<p class="card-text result-p">Temp: ${listItem.main.temp.toFixed(
+          0
+        )}&degF</p>` +
+        `<p class="card-text result-p">Humidity: ${listItem.main.humidity}%</p>` +
+        `</div>`;
+
+      fiveDayResult.append(html);
     }
   }
-  
-  function checkRecentCities(data, zip) {
-    let city = {
-      name: data.city.name,
-      zip: zip,
-    };
-  
-    for (let i = 0; i < recentCities.length; i++) {
-      if (recentCities[i].zip == zip) {
-        recentCities.splice(i, 1);
-      }
-    }
-  
-    if (recentCities.length < 5) {
-      recentCities.unshift(city);
-    } else {
-      recentCities.pop();
-      recentCities.unshift(city);
-    }
-  
-    localStorage.setItem("recent", JSON.stringify(recentCities));
-  }
-  
-  function init() {
-    if (localStorage.getItem("recent") !== null) {
-      let retrievedData = localStorage.getItem("recent");
-      recentCities = JSON.parse(retrievedData);
-      writeRecentCities();
-    }
-  }
-  
-  function populateOneDay(data) {
-    oneDayResult.removeClass("hide");
-    oneDayResult.empty();
-    let cityName = data.city.name;
-    let listItem = data.list[0];
-    let id = data.list[0].weather[0].id;
-    id = id.toString();
-  
-  
-    let html =
-      `<h2>${cityName} ${unixToDate(listItem.dt)}</h2>` +
-      `<p class="result-p">Temp: ${listItem.main.temp.toFixed(0)}&degF</p>` +
-      `<p class="result-p">Humidity: ${listItem.main.humidity}%</p>` +
-      `<p class="result-p">Wind: ${listItem.wind.speed} MPH</p>`;
-  
-    oneDayResult.append(html);
-  }
-  
+}
+
+function unixToHour(timestamp) {
+  let unixTimestamp = timestamp;
+  let date = new Date(unixTimestamp * 1000);
+  let hour = date.getHours();
+  return hour;
+}
+
+function unixToDate(timestamp) {
+  let unixTimestamp = timestamp;
+  let date = new Date(unixTimestamp * 1000);
+  let formatDate = date.toLocaleDateString("en-us");
+  return formatDate;
+}
+
+init();
