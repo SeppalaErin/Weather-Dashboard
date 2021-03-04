4536e57afcd584a644aeda7cd1192a1361cab928
Erin Seppala <aseppala98@gmail.com>
2021-03-03 21:53:13 -0500

    added variables for script.js

---
+++ Assets/script.js
@@ -0,0 +1,10 @@
+let searchBtn = $(".search-button");
+let searchInput = $("#search-input");
+let oneDayResult = $("#one-day-result");
+let listGroup = $("#recent-searches");
+let fiveDayResult = $("#five-day-result");
+let fiveDayTitle = $("#five-day-title");
+
+let recentCities = [];
+
+let apiKey = "5178dc0fce8514b75d7a89420078fe33";
